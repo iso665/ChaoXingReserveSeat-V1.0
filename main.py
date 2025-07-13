7d6b99617564cbb604f3cf26b921381cb39c8eb9
AFen-1 <2608216221@qq.com>
2025-07-13 13:54:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def get_current_dayofweek(action):
 # 全局配置
 SLEEPTIME = 0.2
 ENDTIME = "21:31:00"
-ENABLE_SLIDER = True
+ENABLE_SLIDER = False
 MAX_ATTEMPT = 1  # 增加尝试次数
 RESERVE_TOMORROW = False
 
